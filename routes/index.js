18c9b45495aa97226469d9090c13693eb6a00e86
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2022-06-05 22:02:20 -0500

    Continued work on home page displaying thoughts

--- routes/index.js
+++ routes/index.js
@@ -2,53 +2,49 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
 
-const thought = require('../models/Thought');
+const Thought = require('../models/Thought');
 
 // @desc    Login page
 // @route   GET /
 router.get('/', ensureGuest, (req, res) => {
-  res.render('login', {
-    layout: 'login',
-  });
+    res.render('login', { layout: 'login' });
 });
 
 
 // @desc    Home page
 // @route   GET /home
 router.get('/home', ensureAuth, async(req, res) => {
-  try {
-    // Fetches all thoughts that are by the logged in user
-    const thoughts = await thought.find({ user: req.user.id }).lean();
+    try {
+        // Fetches all thoughts that are by the logged in user
+        const thoughts = await Thought.find({ user: req.user.id })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean();
+        //console.log(thoughts);
+        res.render('home', { thoughts });
 
-    res.render('home', {
-      name: req.user.firstName,
-      handle: req.user.handle,
-      pic: req.user.image,
-      thoughts
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.render('error/500');
-  }
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
 });
 
 
 // @desc    User profile
 // @route   GET /profile
 router.get('/profile', ensureAuth, async(req, res) => {
-  try {
-    // Fetches all thoughts that are by the logged in user
-    const thoughts = await thought.find({ user: req.user.id }).lean();
-    res.render('profile', { 
-      name: req.user.firstName,
-      thoughts
-    });
+    try {
+        // Fetches all thoughts that are by the logged in user
+        const thoughts = await Thought.find({ user: req.user.id }).lean();
+        res.render('profile', { 
+        name: req.user.firstName,
+        thoughts
+        });
 
-  } catch (err) {
-    console.error(err);
-    res.render('error/500');
-  }
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
 });
 
 module.exports = router;
