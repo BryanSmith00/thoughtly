abf930c54627059beaf9a4d08c3cffb7bfa81898
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2022-07-17 21:18:27 -0500

    Fixed following and added unfollowing to profiles

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,12 @@ router.get('/login', ensureGuest, (req, res) => {
 router.get('/', ensureAuth, async (req, res) => {
   try {
     if (req.user.userType === 'user') {
-    // Fetches all thoughts that are by the logged in user
-      const thoughts = await Thought.find({ user: req.user.id, status: 'public' })
+      // Fetches all thoughts that are by the logged in user
+      const thoughts = await Thought.find({ $or: [{ user: req.user.id }, { user: req.user.follows }] })
         .populate('user')
         .sort({ createdAt: 'desc' })
         .lean()
-      // console.log(thoughts);
+
       res.render('user/home', { thoughts })
     } else {
       const admin = req.user
@@ -65,11 +65,17 @@ router.get('/profile', ensureUserAuth, async (req, res) => {
 // @route   GET /user/:handle
 router.get('/user/:handle', ensureAuth, async (req, res) => {
   try {
+    // Profile of the user that is requested
     const profile = await User.findOne({ handle: req.params.handle }).lean()
-    //Grabs all the thoughts posted by the user who's profile is requested and joins them into the thoughts array
+
+    let isFollowed = false
+    isFollowed = req.user.follows.includes(profile._id)
+
+    // Grabs all the thoughts posted by the user who's profile is requested and joins them into the thoughts array
     const thoughts = await Thought.find({ user: profile }).populate('user').sort({ createdAt: 'desc' }).lean()
+
     if (profile.status === 'public' || req.user.follows.includes(profile._id)) {
-      res.render('userProfile', { profile, thoughts })
+      res.render('userProfile', { isFollowed, profile, thoughts })
     } else {
       // TODO: Remove profile injection when request follow is implemented
       res.render('denied', { profile })
@@ -91,7 +97,7 @@ router.get('/discover', ensureAuth, async (req, res) => {
   }
 })
 
-// @desc    follow user account
+// @desc    follow user
 // @route   PUT /follow/:id
 router.put('/follow/:id', ensureUserAuth, async (req, res) => {
   try {
@@ -112,7 +118,35 @@ router.put('/follow/:id', ensureUserAuth, async (req, res) => {
       await current.save()
     }
 
-    res.redirect('/')
+    res.redirect('back')
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
+
+// @desc    unfollow user
+// @route   PUT /unfollow/:id
+router.put('/unfollow/:id', ensureUserAuth, async (req, res) => {
+  try {
+    const userToUnfollow = await User.findById(req.params.id).lean()
+
+    if (!userToUnfollow) {
+      return res.render('error/404')
+    }
+
+    const current = await User.findById(req.user.id)
+
+    if (!current) {
+      return res.render('error/404')
+    }
+
+    if (current.follows.includes(req.params.id)) {
+      current.follows.pop(req.params.id)
+      await current.save()
+    }
+
+    res.redirect('back')
   } catch (err) {
     console.error(err)
     return res.render('error/500')
