eabac001c74bedb93e4d908efe6a8dd294dec7c9
Bryan Smith <46660845+BryanSmith00@users.noreply.github.com>
2023-08-28 23:21:24 -0500

    mern test with signup endpoint

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
+
+// Load env config file
+dotenv.config({ path: "./config.env" });
+const PORT = process.env.PORT || 3000
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on ${process.env.NODE_ENV} mode on http://localhost:${PORT}/`
+  );
+});
+
+app.use(
+  cors({
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/", authRoute);
